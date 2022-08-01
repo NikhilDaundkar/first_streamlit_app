c1c6d5248ec950d559ce415d0e1b6182ebf53278
Nikhil Daundkar <110382588+NikhilDaundkar@users.noreply.github.com>
2022-08-01 18:36:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner Nikhil')
 streamlit.header('Breakfast Menu')
-
+streamlit.text('Omega 3 & Buleberry outmeal')
+streamlit.text('Kale, Spinach & Roket Smoothie')
+streamlit.text('Haed-Boil free-Range Egg')
